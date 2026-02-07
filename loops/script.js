d01eec670e75ab57da8a6dd6f9d5135629efcf9b
Rigor <60898867+igor007-cyber@users.noreply.github.com>
2026-02-07 00:30:47 +0000

    codigo

---
+++ loops/script.js
@@ -0,0 +1,35 @@
+// console.log("ola mundo");
+
+// for (let numero = 1; numero<=10; numero++){
+//     console.log(numero);
+// }
+
+// let x = 1
+// while (x<=10) {
+//     console.log(x)
+//     x++;
+// }
+
+// let i = 1
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i<=3);
+
+for (let index = 0; index < 6; index++) {
+    
+    let linha = "";
+    for(let j = 1; j <= index; j++){
+        linha += "* ";
+    }
+    console.log(linha);
+    
+}
+
+const fruite = ["apple", "orange", "banana"];
+for (const element of fruite) {
+    console.log(element);
+    
+}
+
